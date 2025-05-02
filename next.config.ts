dd05de2ddf98a4c512ac3607f95029389abc0efc
aHasnat <hasnat56703@gmail.com>
2025-05-02 17:55:35 +0600

    Fix: next config

--- next.config.ts
+++ next.config.ts
@@ -2,16 +2,6 @@ import type { NextConfig } from "next";
 
 // Define the Next.js configuration object
 const nextConfig: NextConfig = {
-  // Customize the Webpack configuration
-  webpack(config) {
-    // Add a rule to handle SVG files using @svgr/webpack
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ['@svgr/webpack'],
-    });
-
-    return config;
-  },
   // Configure image handling for remote patterns
   images: {
     remotePatterns: [
